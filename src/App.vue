4176e06dff4267dd6925207bdf5a9723b8c385d2
BlueFeet <b1uefeet.dev@gmail.com>
2024-03-03 16:32:01 -0500

    data fix

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
           alt="Cannot access to resource"
         /></router-link>
         
-        <navBar id="idNavBar" :tipo="E"/>
+        <navBar id="idNavBar" tipo="E"/>
       </header>
 
         <router-view class="router"/>
@@ -39,11 +39,6 @@ export default {
     navBar,
     router,
   },
-  data(){
-    return{
-      tipo
-    };
-  }
 };
 </script>
 
