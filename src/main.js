5c941b01cae7ea4abe517e92a440175d2b927cad
Reymon <rahidalgob@uce.edu.ec>
2024-03-04 14:42:22 -0500

    MessagBox-Login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
 
-import router from './router/router'
-createApp(App).use(router).mount('#app')
+import router from './router/router';
+createApp(App).use(router).use(ElementPlus).mount('#app');
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -21,49 +21,71 @@
 </template>
 
 <script>
+import { ElMessageBox } from "element-plus";
 export default {
   data() {
     return {
       cedula: null,
-      contrasenia:null,
-
+      contrasenia: null,
     };
   },
 
   methods: {
     async inicio() {
-        /* VERIFICAR LOS ATRIBUTOS EN EL BACK END */
+      /* VERIFICAR LOS ATRIBUTOS EN EL BACK END */
       const clienteBody = {
         cedula: this.cedula,
-        contrasenia:this.contrasenia,
+        contrasenia: this.contrasenia,
       };
 
-      if(this.cedula === 'admin' && this.contrasenia === 'admin'){
-        this.$emit('cambio-tipo', 'E');
-         await this.redireccionar()
-      }else{
-        if(await inicioFachada(clienteBody)){
-          this.$emit('cambio-tipo', 'C')
-          await this.redireccionar()
-        }
-        else{
-          //mensaje de revisa ususua y contraseña
-        }
+      if (this.cedula === "admin" && this.contrasenia === "admin") {
+        this.$emit("cambio-tipo", "E");
+        await this.redireccionar();
+      } else {
+        this.mostrarMensaje();
+        /* if (await inicioFachada(clienteBody)) {
+          this.$emit("cambio-tipo", "C");
+          await this.redireccionar();
+        } else { */
+        // mensaje de revisa usuario y contraseña
+        console.log("Revisa usuario y contraseña");
+        // }
       }
-      
-      
-
     },
-    redireccionar(){
+
+    redireccionar() {
       this.$router.push({ path: "/inicio" });
-    }
+    },
+
+    mostrarMensaje() {
+      ElMessageBox.alert(
+        "Credenciales Incorrectas",
+        "Revise sus credenciales e intente nuevamente",
+        {
+          confirmButtonText: "Ok",
+          type: "error",
+          position: "center",
+          customClass:"messageBox",
+          callback: () => {
+            // Acciones después de hacer clic en "Aceptar"
+            console.log("Mensaje aceptado");
+          },
+        }
+      );
+    },
   },
 };
-
-
 </script>
 
 <style scoped>
+
+.messageBox{
+  background-color: #833e3e!important;
+  color:rgb(221, 23, 23)3!important; 
+  border: 1px solid #1c0fcf; 
+  padding: 10px; 
+  width: 10%!important; 
+}
 h1 {
   align-items: center;
   font-style: italic;
